ae4b7cd0a9a208a4d843a9499aa054563dc337ca
Kamil Mazur <maz.kamil@gmail.com>
2019-03-11 20:53:48 +0100

    Task3

---
+++ Excercise 1/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        /*
+         * Check what capabilities are offered by your favourite technology (e.g. Django, RoR, Spring) to
+protect the secret key (in the symmetric encryption) and the private key (in the public key encryption). Is this the same way or are there any differences?
+         */
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
