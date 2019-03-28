ac4491ff01c6909d043d718a5ee855f6834706f6
Kamil Mazur <maz.kamil@gmail.com>
2019-03-28 23:31:58 +0100

    roles and users

---
+++ Excercise3/RolesAndStuff/RolesAndStuff/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using RolesAndStuff.Models;
+using RolesAndStuff.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RolesAndStuff.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Employee")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        public ActionResult ManageRoles()
+        {
+            {
+                var usersWithRoles = (from user in _context.Users
+                                      select new
+                                      {
+                                          UserId = user.Id,
+                                          Username = user.UserName,
+                                          Email = user.Email,
+                                          RoleNames = (from userRole in user.Roles
+                                                       join role in _context.Roles on userRole.RoleId
+                                                       equals role.Id
+                                                       select role.Name).ToList()
+                                      }).ToList().Select(p => new UsersRoleViewModel()
+
+                                      {
+                                          UserId = p.UserId,
+                                          Username = p.Username,
+                                          Email = p.Email,
+                                          Role = string.Join(",", p.RoleNames)
+                                      }).ToList();
+
+
+                return View(usersWithRoles);
+            }
+        }
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public ActionResult ManageRoles( UsersRoleViewModel valu2e)
+        {
+            var roleNames = (from userRole in _context.Roles
+                             select userRole.Name).ToList();
+
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+            if (valu2e.Role != null)
+                UserManager.RemoveFromRole(valu2e.UserId, valu2e.Role);
+            var index = roleNames.FindIndex(x => x == valu2e.Role);
+            var result1 = UserManager.AddToRole(valu2e.UserId, roleNames[(index+1)%roleNames.Count]);
+            return ManageRoles();
+        }
+
+        [Authorize(Roles = "Manager")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Excercise3/RolesAndStuff/RolesAndStuff/Startup.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using RolesAndStuff.Models;
+
+[assembly: OwinStartupAttribute(typeof(RolesAndStuff.Startup))]
+namespace RolesAndStuff
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRolesandUsers();
+        }
+
+        public void CreateRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool    
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                   
+
+                var user = new ApplicationUser();
+                user.UserName = "admin@gmail.com";
+                user.Email = "admin@gmail.com";
+
+                string userPWD = "Admin.123";
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin    
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+            // creating Creating Manager role     
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating Employee role     
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+
+            }
+        }
+
+
+    }
+}
